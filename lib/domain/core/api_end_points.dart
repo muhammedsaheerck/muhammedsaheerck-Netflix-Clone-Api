63bafdc33722c9f87fceb05217c8b0583b44938f
Muhammed Saheer ck <saheerchelakkodan@gmail.com>
2022-12-03 12:55:39 +0530

    Netflix Clone completed

---
+++ lib/domain/core/api_end_points.dart
@@ -0,0 +1,13 @@
+import 'package:netflix_clone/core/strings.dart';
+import 'package:netflix_clone/infrastructure/api_key.dart';
+
+
+class ApiEndPoints {
+  static const downloads = "$kBaseUrl/trending/all/day?api_key=$apiKey";
+  static const search = "$kBaseUrl/search/movie?api_key=$apiKey";
+
+  static const hotAndNewMovie = '$kBaseUrl/discover/movie?api_key=$apiKey';
+  static const hotAndNewTV = '$kBaseUrl/discover/tv?api_key=$apiKey';
+  
+  static const homeTopRated = '$kBaseUrl/movie/top_rated?api_key=$apiKey';
+}
